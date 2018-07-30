00778cc6ddbabb78d4135353d49d9ee094633d5b
jetstreamokayasu <tokotsuku2@outlook.jp>
2018-07-30 23:31:40 +0900

    作った平面へ写像する関数修正

--- square.R
+++ square.R
@@ -26,80 +26,51 @@ t<-runif(100, -5, 5)
 
 vic3<-get.vicinity(torus.dist, 3, 10)
 
-#任意の点(center)の近傍による最小二乗法で平面を作る
-square.method<-function(vic, center){
-
-  A<-rbind(x[center,], x[vic[which(vic[,2]!=center), 2], ])
-  A<-rbind(A, x[vic[which(vic[,1]!=center), 1], ])
-  z<-A[,3]
-  A[,3]<-1
-  
-  a<-solve(t(A)%*%A)%*%t(A)%*%z
-  
-  return(a)
-  
-}
-
 a3<-square.method(vic3, 3)
+
+result<-lm(x[vic3s, 3]~x[vic3s, 1]+x[vic3s, 2])
 
 #plot3d(s, t, v<-a3[1,1]*s+a3[2,1]*t+a3[3,1], col="red")
 sur<-cbind(s, t, v<-a3[1]*s+a3[2]*t+a3[3])
-points3d(sur, col="red")
+points3d(sur, col=4)
 # test.x<-rbind(x, sur)
 # plot3d(test.x)
 # aspect3d("iso")
 
-#近傍の点から作られた平面へ下した垂線の足を求める
-mapping<-function(vic, center, coe){
-  
-  vics<-sapply(1:length(vic[,2]), function(t){
-    
-    if(vic[t,2]==center) return(vic[t, 1])
-    else return(vic[t, 2])
-    
-  })
-  
-  vics<-c(center, vics)
-  
-  O<-x[center, ]
-  O[3]<-coe[1]*O[1]+coe[2]*O[2]+coe[3]
-  
-  if(vic[10,2]==center) P<-x[vic[10, 1], ]
-  else P<-x[vic[10, 2], ]
-  
-  P[3]<-coe[1]*P[1]+coe[2]*P[2]+coe[3]
-  
-  if(vic[5,2]==center) Q<-x[vic[5, 1], ]
-  else Q<-x[vic[5, 2], ]
-  
-  Q[3]<-coe[1]*Q[1]+coe[2]*Q[2]+coe[3]
-  
-  vec.OP<-O-P
-  vec.OQ<-O-Q
-  
-  R<-cbind(vec.OP, vec.OQ)
-  
-  foot<-sapply(vics, function(a){
-    
-    st<-solve(t(R)%*%R)%*%(t(R)%*%cbind(x[a, ]))
-    return(c(a,(R%*%st)+O))
-    
-  })
-  
-  #st<-solve(t(R)%*%R)%*%(t(R)%*%cbind(x[center, ]))
-  
-  #foot<-(R%*%st)+O
-  
-  foot<-t(foot)
-  debugText(foot)
-  rownames(foot)<-NULL
-  debugText(foot)
-  colnames(foot)<-c("origin", "x", "y", "z")
-  debugText(foot)
-  return(foot)
-  
-}
-
 foot3<-mapping(vic3, 3, a3)
 points3d(foot3[,2:4], col=3)
 
+
+plot3d(x[-vic3[,2],])
+aspect3d("iso")
+points3d(x[vic3[,2],], col=3)
+
+vic3s<-line.vics(3, vic3)
+
+feet3<-mapping2(vic3, 3, a3)
+points3d(feet3[-c(1, 11), 2:4], col=2)
+points3d(feet3[c(1, 11), 2:4], col="pink")
+
+interpolation<-function(centr, feet, coe){
+  
+  q<-rep(0, 3)
+  c<-feet[1, 2:4]
+  p<-feet[length(feet[,1]), 2:4]
+  r<-p-c
+  
+  q[1]<-feet[(length(feet3[,1])%/%2)+1, "x"]
+  
+  q[2]<-((-q[1])*(r[1]+r[3]*coe[1])+sum(r*c)-r[3]*coe[3])/(r[2]+coe[2]*r[3])
+  
+  q[3]<-coe[1]*q[1]+coe[2]*q[2]+coe[3]
+  
+  d<-sqrt(sum(r^2))
+  e1<-r/d
+  e2<-(q-c)/sqrt(sum((q-c)^2))
+  
+  return(q)
+  
+}
+
+q3<-interpolation(3, feet3, a3)
+points3d(rbind(c(0,0,0), q3), col="orange")
--- square_func.R
+++ square_func.R
@@ -34,3 +34,107 @@ get.vicinity<-function(dis, center, nvic){
   return(vic)
   
 }
+
+#任意の点(center)の近傍による最小二乗法で平面を作る
+square.method<-function(vic, center){
+  
+  A<-rbind(x[center,], x[vic[which(vic[,2]!=center), 2], ])
+  A<-rbind(A, x[vic[which(vic[,1]!=center), 1], ])
+  z<-A[,3]
+  A[,3]<-1
+  
+  a<-solve(t(A)%*%A)%*%t(A)%*%z
+  
+  return(a)
+  
+}
+
+#近傍の点から作られた平面へ下した垂線の足を求める
+# mapping<-function(vic, center, coe){
+#   
+#   vics<-sapply(1:length(vic[,2]), function(t){
+#     
+#     if(vic[t,2]==center) return(vic[t, 1])
+#     else return(vic[t, 2])
+#     
+#   })
+#   
+#   vics<-c(center, vics)
+#   
+#   O<-x[center, ]
+#   O[3]<-coe[1]*O[1]+coe[2]*O[2]+coe[3]
+#   
+#   if(vic[10,2]==center) P<-x[vic[10, 1], ]
+#   else P<-x[vic[10, 2], ]
+#   
+#   P[3]<-coe[1]*P[1]+coe[2]*P[2]+coe[3]
+#   
+#   if(vic[5,2]==center) Q<-x[vic[5, 1], ]
+#   else Q<-x[vic[5, 2], ]
+#   
+#   Q[3]<-coe[1]*Q[1]+coe[2]*Q[2]+coe[3]
+#   
+#   vec.OP<-O-P
+#   vec.OQ<-O-Q
+#   
+#   R<-cbind(vec.OP, vec.OQ)
+#   
+#   foot<-sapply(vics, function(a){
+#     
+#     st<-solve(t(R)%*%R)%*%(t(R)%*%cbind(x[a, ]))
+#     return(c(a,(R%*%st)+O))
+#     
+#   })
+#   
+#   #st<-solve(t(R)%*%R)%*%(t(R)%*%cbind(x[center, ]))
+#   
+#   #foot<-(R%*%st)+O
+#   
+#   foot<-t(foot)
+#   debugText(foot)
+#   rownames(foot)<-NULL
+#   debugText(foot)
+#   colnames(foot)<-c("origin", "x", "y", "z")
+#   debugText(foot)
+#   return(foot)
+#   
+# }
+
+#任意の点(centr)の近傍点の一覧を作る関数
+line.vics<-function(centr, vic){
+  
+  vics<-sapply(1:length(vic[,2]), function(t){
+    
+    if(vic[t,2]==centr) return(vic[t, 1])
+    else return(vic[t, 2])
+    
+  })
+  
+  names(vics)<-NULL
+  
+  vics<-c(centr, vics)
+  
+  return(vics)
+  
+}
+
+#近傍の点から作られた平面への射影点を求める関数
+mapping2<-function(vic, center, coe){
+  
+  vics<-line.vics(center, vic)
+  
+  feet<-sapply(vics, function(p){
+    
+    t<-(x[p, 3]-coe[1]*x[p, 1]-coe[2]*x[p, 2]-coe[3])/(sum((c(coe[1:2], 1))^2))
+    
+    return(c(p, coe[1]*t+x[p, 1], coe[2]*t+x[p, 2], -t+x[p, 3]))
+    
+  })
+  
+  feet<-t(feet)
+  debugText(feet)
+  colnames(feet)<-c("origin", "x", "y", "z")
+  debugText(feet)
+  return(feet)
+  
+}
